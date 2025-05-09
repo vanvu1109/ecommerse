0f5191840055d546db5204498d5952453cf346e2
vanvu1109 <tuilavanvuday@gmail.com>
2025-05-09 17:16:45 +0700

    Mô tả ngắn gọn thay đổi

--- do-ecommerse/src/components/Header/BoxIcon/BoxIcon.jsx
+++ do-ecommerse/src/components/Header/BoxIcon/BoxIcon.jsx
@@ -1,8 +1,29 @@
 import styles from '../styles.module.scss';
+import fbIcon from '@icons/svgs/fbIcon.svg';
+import ytbIcon from '@icons/svgs/ytbIcon.svg';
+import intaIcon from '@icons/svgs/intaIcon.svg';
 
 function BoxIcon({ type, href}) {
+    const { boxIcon } = styles;
+
+    const handleRanderIcon = (type) => {
+        switch (type) {
+            case 'fb':
+                return fbIcon
+                
+            case 'ytb':
+                return ytbIcon
+                
+            case 'ins':
+                return intaIcon
+                
+        }
+    }; 
+
     return (
-        <div>{type}</div>
+        <div className={boxIcon}>
+            <img src={handleRanderIcon(type)} alt={type} />
+        </div>
     )
 };
 export default BoxIcon;
--- do-ecommerse/src/components/Header/Header.jsx
+++ do-ecommerse/src/components/Header/Header.jsx
@@ -1,16 +1,56 @@
 import BoxIcon from "./BoxIcon/BoxIcon";
+import Menu from "./Menu/Menu";
+import { dataBoxICon, dataMenu } from './constant';
+import styles from './styles.module.scss';
+import Logo from '@icons/images/Logo.png';
+
+
+import reloadIcon from '@icons/svgs/reloadIcon.svg'
+import heartIcon from '@icons/svgs/heartIcon.svg'
+import cartIcon from '@icons/svgs/cartIcon.svg'
+
 
 function MyHeader() {
+    const { containerBoxIcon, containerMenu, containerHeader, containerBox} = styles;
     return (  
-       <div>
-        <div>
-            <div>
-                <BoxIcon type={'fb'} href={''}/>
+       <div className={containerHeader}>
+        <div className={containerBox}>
+            <div className={containerBoxIcon}>
+                {
+                   dataBoxICon.map(( item )=> {
+                    return <BoxIcon type={item.type} href={item.href}/> 
+                   })
+                }
             </div>
-            <div></div>
+            <div className={containerMenu}>
+                {
+                    dataMenu.slice(0, 3).map(( item ) => {
+                        return <Menu content={item.content} href={item.href}/>
+                    })
+                }
+            </div>
         </div>
-        <div></div>
-        <div></div>
+        <div>
+            <img src={Logo} alt="logo" style={{
+                width: "153px",
+                height: "53px"
+            }} />
+        </div>
+        <div className={containerBox}> 
+                <div className={containerMenu}>
+                {
+                    dataMenu.slice(3, dataMenu.length).map(( item ) => {
+                        return <Menu content={item.content} href={item.href}/>
+                    })
+                }
+
+                <div className={containerBoxIcon}>
+                    <img width={26} height={26} src={reloadIcon} alt={reloadIcon} />
+                    <img width={26} height={26} src={heartIcon} alt={heartIcon} />
+                    <img width={26} height={26} src={cartIcon} alt={cartIcon} />
+                </div>
+        </div>
+        </div>
        </div>
     );
 }
